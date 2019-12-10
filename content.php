2be53795c95673f2fd3bd65dc9f5bb4806565e63
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-10 10:36:10 +0100

    content.php : base of the todolist

---
+++ content.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>To Do list</title>
+    <meta name="description" content="Brice & Denis' To Do list for BeCode">
+</head>
+<body>
+    <h1>To Do List</h1>
+    <h2>To Do:</h2>
+    <ul>
+        <?php
+/* Call here the function for the "to do list" */
+?>
+    </ul>
+    <h2>Done:</h2>
+    <ul>
+        <?php
+/* Call here the function for the "done list" */
+?>
+    </ul>
+    <h2>Add a task:</h2>
+    <form action="" method="post">
+    </form>
+</body>
+</html>
