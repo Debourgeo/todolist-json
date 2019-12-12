2c942bd6028a376a8605611d8ef9b522fac93569
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-12 10:18:05 +0100

    function.php : recho list of done, get the data ok

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -12,6 +12,16 @@ function echo_list_of_done()
 {
     // Read the data of Done
 
+    // this function gets all the data from the json in one string
+    $str = file_get_contents('../json/todo.json');
+
+    // This function decode the json string to get an object.
+    // The true parameter is to convert all object into an associative array.
+    // Having an associativ array is usefull to iterate over it.
+    $data = json_decode($str, true);
+
     // loop to echo the data of Done
 
 }
+
+echo_list_of_done();
