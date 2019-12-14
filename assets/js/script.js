90fdb5921170742913687b57cefb575c393453f9
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2019-12-15 00:08:36 +0100

    save work before experimentation with json.stringify

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,22 +9,45 @@
 
 /*
 
-Prevent submission by button
-Submission by ticking the checkbox
+Prevent submission by button                         ok
+Submission by ticking the checkbox          to do
 
 Prevent the screen refresh
 Update the data 
 
 */
 
-let checkBoxForm = document.getElementById('checkBoxForm');
+const checkBoxForm = document.getElementById('checkBoxForm');
 
 // Desactivate the submit button and hiding it.
 // The form will be submit by ticking the checkbox
-let button = document.getElementById('submit_add_archive');
+const button = document.getElementById('submit_add_archive');
 button.addEventListener("click", e => e.preventDefault());
 button.style.display = "none";
 
+const arrayOfTodo = document.querySelectorAll(".todo");
+// console.log(arrayOfTodo);
 
 
-// checkBoxForm.addEventListener("submit", e => e.preventDefault());
+const ticking = async () => {
+    checkBoxForm.submit()
+    // try {
+    //     let modificationRequest = await fetch(
+    //         "http://0.0.0.0:8000/todolist-json/assets/php/Form_task_to_archive.php", {
+    //             method: "POST",
+    //             headers: {
+    //                 "Content-type": "multipart/form-data",
+    //             },
+    //             body: new FormData(document.getElementById("checkBoxForm"))
+    //         }
+    //     )
+    // } catch (error) {
+    //     console.error(error);
+    // }
+}
+
+
+
+arrayOfTodo.forEach(checkBoxTodo => {
+    checkBoxTodo.addEventListener("click", () => ticking());
+});
